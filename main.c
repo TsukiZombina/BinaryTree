02c7c1c484edf879cb2b5939be3a45f121488720
TsukiZombina <ammy_zmmn@hotmail.com>
2017-10-28 17:42:16 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct node{
+    int datum;
+    int* lchild, rchild;
+}
+
+typedef struct node Node;
+
+void create_tree();
+void inorder_traverse();
+void
+int main(){
+
+    return 0;
+}
